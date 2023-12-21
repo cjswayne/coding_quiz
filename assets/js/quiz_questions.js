7096e506bbc35f9ee6512a8e0f93a28bf017363f
cjswayne <134882908+cjswayne@users.noreply.github.com>
2023-12-21 10:48:41 -0800

    adding in quiz functionality

---
+++ assets/js/quiz_questions.js
@@ -0,0 +1,198 @@
+let quizQuestions = [
+    {
+        question: "Which HTML5 element is used for defining a footer for a document or section?",
+        answer: "<footer>",
+        answers: [
+            "<footer>",
+            "<bottom>",
+            "<div>",
+            "<section>"
+        ]
+    },
+
+    {
+        question: "What is the purpose of the 'async' attribute in a <script> tag?",
+        answer: "It loads the script asynchronously with the rest of the page",
+        answers: [
+            "It defers script execution until page load",
+            "It pauses script execution",
+            "It loads the script synchronously",
+            "It loads the script asynchronously with the rest of the page"
+        ]
+    }
+   
+    ,
+    {
+        question: "In Node.js, which module is used to handle HTTP requests and responses?",
+        answer: "http",
+        answers: [
+            "request",
+            "http",
+            "net",
+            "socket"
+        ]
+    },
+    {
+        question: "What does the 304 status code in HTTP signify?",
+        answer: "Not Modified",
+        answers: [
+            "Bad Request",
+            "Not Modified",
+            "Forbidden",
+            "Not Found"
+        ]
+    },
+    {
+        question: "In React, what is the method to change a component's state?",
+        answer: "setState()",
+        answers: [
+            "changeState()",
+            "modifyState()",
+            "updateState()",
+            "setState()"
+        ]
+    }
+    ,
+    {
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        answers: [
+            "Simple Query Language",
+            "Structured Query Language",
+            "Standard Query Language",
+            "Secure Query Language"
+        ]
+    },
+    {
+        question: "In MongoDB, what is a 'Document' equivalent to in SQL?",
+        answer: "A row in a table",
+        answers: [
+            "A table",
+            "A database",
+            "A row in a table",
+            "A column in a table"
+        ]
+    }
+    // ,
+    // {
+    //     question: "Which CSS layout model introduces a two-dimensional grid system to the web?",
+    //     answer: "CSS Grid Layout",
+    //     answers: [
+    //         "Flexbox",
+    //         "CSS Grid Layout",
+    //         "Box Model",
+    //         "Positioning"
+    //     ]
+    // },
+    // {
+    //     question: "What is the purpose of Docker in development?",
+    //     answer: "Creating, deploying, and running applications in isolated environments (containers)",
+    //     answers: [
+    //         "Version control",
+    //         "Text editing",
+    //         "Creating, deploying, and running applications in isolated environments (containers)",
+    //         "Browser testing"
+    //     ]
+    // },
+    // {
+    //     question: "What does REST stand for in the context of API design?",
+    //     answer: "Representational State Transfer",
+    //     answers: [
+    //         "Real-time Simple Transfer",
+    //         "Representational State Transfer",
+    //         "Reliable Session Transfer",
+    //         "Remote Server Transfer"
+    //     ]
+    // },
+    // {
+    //     question: "In Angular, what is a Directive?",
+    //     answer: "A class with a @Directive decorator that extends HTML behavior",
+    //     answers: [
+    //         "A method to route paths",
+    //         "A server-side script",
+    //         "A CSS preprocessor",
+    //         "A class with a @Directive decorator that extends HTML behavior"
+    //     ]
+    // },
+    // {
+    //     question: "What is the main purpose of a JWT (JSON Web Token)?",
+    //     answer: "To securely transmit information between parties as a JSON object",
+    //     answers: [
+    //         "To encrypt passwords",
+    //         "To manage user sessions",
+    //         "To securely transmit information between parties as a JSON object",
+    //         "To store data in local storage"
+    //     ]
+    // },
+    // {
+    //     question: "In Redux, what is an 'Action'?",
+    //     answer: "An object that describes what happened",
+    //     answers: [
+    //         "A function that returns the new state",
+    //         "A method to dispatch state changes",
+    //         "An object that describes what happened",
+    //         "A component that handles state"
+    //     ]
+    // },
+    // {
+    //     question: "What is the main use of TypeScript in web development?",
+    //     answer: "To provide static typing for JavaScript",
+    //     answers: [
+    //         "To compile code to machine language",
+    //         "To provide static typing for JavaScript",
+    //         "To manage server-side scripting",
+    //         "To enhance CSS styling"
+    //     ]
+    // },
+    // {
+    //     question: "In Git, what is the command to create a new branch?",
+    //     answer: "git branch [branch-name]",
+    //     answers: [
+    //         "git make branch [branch-name]",
+    //         "git new branch [branch-name]",
+    //         "git branch [branch-name]",
+    //         "git create [branch-name]"
+    //     ]
+    // },
+    // {
+    //     question: "What does 'CI/CD' stand for in DevOps?",
+    //     answer: "Continuous Integration/Continuous Delivery",
+    //     answers: [
+    //         "Continuous Integration/Continuous Deployment",
+    //         "Continuous Implementation/Continuous Development",
+    //         "Continuous Integration/Continuous Delivery",
+    //         "Constant Integration/Constant Deployment"
+    //     ]
+    // },
+    // {
+    //     question: "In web development, what does 'SEO' stand for?",
+    //     answer: "Search Engine Optimization",
+    //     answers: [
+    //         "Search Engine Optimization",
+    //         "Server Endpoint Output",
+    //         "Simple Element Object",
+    //         "Secure Encryption Operation"
+    //     ]
+    // },
+    // {
+    //     question: "What is a 'Promise' in JavaScript?",
+    //     answer: "An object representing the eventual completion or failure of an asynchronous operation",
+    //     answers: [
+    //         "A data type similar to an array",
+    //         "A function that executes conditionally",
+    //         "A loop that runs until a condition is met",
+    //         "An object representing the eventual completion or failure of an asynchronous operation"
+    //     ]
+    // },
+    // {
+    //     question: "In web security, what is 'XSS'?",
+    //     answer: "Cross-Site Scripting",
+    //     answers: [
+    //         "Cross-Server Security",
+    //         "Cross-Site Scripting",
+    //         "Extended Style Sheets",
+    //         "Exclusive Server Setup"
+    //     ]
+    // }
+    
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,342 +11,240 @@ Need to move where incorrect and correct show,
 - do normal submission checklist
 */
 
-
-
-
-// Function to inject question text
-let questionNum = 0;
-
-
+// function to add listeners to answer buttons
 function addAnswerListener(correct) {
-    let text = document.getElementById('high-scores');
-    return function(event) {
-        if(correct) {
-            questionNum++;
-            text.innerText = "Correct!";
-            text.style.color = "green";
+  return function (event) {
+    correctStyle(correct)
+    if (totalTime < timePenalty || questionNum == quizQuestions.length) { // needs to be penalty
+      score = totalTime;
+      initials();
+      endGame();
+      totalTime = 0;
+    } else {
+      // This line will inject the next question
+      injectQuestion(quizQuestions[questionNum], event);
+    }
+  };
+}
+
+// function to display incorrect or correct message
+function correctStyle(correct){
+    if(questionCard.classList.contains('hide')){
+      text.classList.remove('unhide');
+      text.classList.add('hide');
+        text.style.display = "none";
+    } else if(questionCard.classList.contains('unhide')){
+      text.textContent = "";
+      text.classList.remove('hide');
+      text.classList.add('unhide');
+      text.style.display = "block";
+        questionNum++;
+        if(!correct){correct=false;}
+        if(correct){
+            text.textContent = "Correct!";
+            text.classList.add('correct');
         } else {
-            questionNum++;
-            text.innerText = "Incorrect!";
-            text.style.color = "red";
+            text.textContent = "Incorrect!";
+            text.classList.add('incorrect');
             subtractTime();
         }
-        console.log(questionNum);
-        console.log(quizQuestions.length)
-    
-        if(questionNum == quizQuestions.length){
-            initials();
-    
-        } else {
-        // This line will inject the next question
-        injectQuestion(quizQuestions[questionNum], quizQuestions[questionNum].answers);
-        }
-    };
+        setTimeout(function(){
+          text.classList.remove('unhide');
+          text.classList.add('hide');
+
+        }, 1000);
+    }
 }
 
 
-function injectQuestion(question, questionAnswers){
 
+// function to inject question
+function injectQuestion(question, event) {
+  console.log(event);
+  let questionText = document.querySelector("#question");
+  let answers = question.answers;
+  let answerButtons = document.querySelectorAll(".answer");
+  let correctAnswer = question.answer;
+  questionText.textContent = question.question;
 
-    let correctAnswerButton = "";
-    let questionText = document.querySelector("#question");
-    let answers = questionAnswers;
-    let answerButtons = document.querySelectorAll(".answer");
-    let correctAnswer = question.answer;
-    questionText.innerText = question.question;
+  answerButtons.forEach((button) => {
+    let clone = button.cloneNode(true);
+    button.parentNode.replaceChild(clone, button);
+  });
 
-    answerButtons.forEach(button => {
-        let clone = button.cloneNode(true);
-        button.parentNode.replaceChild(clone, button);
-    });
+  answers.forEach(function (answer, index) {
+    let answerTextId = `question${index + 1}`;
+    let answerButtonId = `button-question${index + 1}`;
+    let answerText = document.getElementById(answerTextId);
+    let answerButton = document.getElementById(answerButtonId);
 
-    answers.forEach(function(answer, index){
-        let answerTextId = `question${index+1}`;
-        let answerButtonId = `button-question${index+1}`;
-        let answerText = document.getElementById(answerTextId);
-        let answerButton = document.getElementById(answerButtonId);
+    answerText.textContent = answer;
+    if (correctAnswer === answer) {
+      answerButton.style.backgroundColor = "green";
+    } else {
+      answerButton.style.backgroundColor = "blue";
+    }
 
-        answerText.innerText = answer;
-        answerButton.addEventListener('click', addAnswerListener(correctAnswer === answer));
-    });
-
+    answerButton.addEventListener(
+      "click",
+      addAnswerListener(correctAnswer === answer)
+    );
+  });
 }
 
+// function to end game
+function endGame() {
+  correctStyle();
+  timerDisplay.textContent = 0;
+  totalTime;
+}
 
+// function to reset the questions
+function resetQuestions() {
+  firstQuestion = 0;
+  injectQuestion(quizQuestions[0]);
+}
 
 //function to start timer
-function startTimer(){
-    let timer = setInterval(function(){
-        totalTime -= 1;
-        timerDisplay.innerText = totalTime;
+function startTimer(timer) {
+  timer = setInterval(function () {
+    if (totalTime <= 0) {
+      clearInterval(timer);
+      timer;
+      if (questionCard.classList.contains('unhide')){
+        score = 0;
+        initials();
 
-        if (totalTime <= 0){
-            clearInterval(timer);
-            console.log('done');
-        }
-    }, 1000);
+        endGame();  
+      }
+
+    console.log(totalTime);
+    } else if (questionNum != quizQuestions.length && questionCard.classList.contains('unhide')) {
+      timerDisplay.textContent = totalTime;
+      totalTime -= 1;
+    }
+  }, 1000);
 }
+
 //function to subtract time
-function subtractTime(){
-    totalTime -= timePenalty;
-    console.log('time deducted')
+function subtractTime() {
+  totalTime -= timePenalty;
 }
 
 //function to enter initials
-function initials(){
-    let score = totalTime;
-    const initialsContainer = document.getElementById('initials-container');
-    const questionContainer = document.getElementById('quiz-question-container');
+function initials() {
+  const scoreText = document.getElementById("score");
+  scoreText.textContent = score;
+  timerDisplay.textContent = 0;
+  hideOtherCards(initialsCard);
+}
 
-    totalTime = 1;
-    timerDisplay.innerText = totalTime;
-    questionContainer.style.display = "none";
-    initialsContainer.style.display = "block";
+// Function to reset game
+function restartQuiz() {
+  questionNum = 0;
+  totalTime = startingTime;
+  timerDisplay.textContent = totalTime +1;
 
-    const scoreText = document.getElementById("score");
-    console.log(scoreText);
-
-    scoreText.innerText = score;
-
+  startTimer(timer);
+  hideOtherCards(questionCard);
 
 }
-let quizQuestions = [
-    {
-        question: "Which HTML5 element is used for defining a footer for a document or section?",
-        answer: "<footer>",
-        answers: [
-            "<footer>",
-            "<bottom>",
-            "<div>",
-            "<section>"
-        ]
-    },
 
-    {
-        question: "What is the purpose of the 'async' attribute in a <script> tag?",
-        answer: "It loads the script asynchronously with the rest of the page",
-        answers: [
-            "It defers script execution until page load",
-            "It pauses script execution",
-            "It loads the script synchronously",
-            "It loads the script asynchronously with the rest of the page"
-        ]
-    },
-    // {
-    //     question: "In Node.js, which module is used to handle HTTP requests and responses?",
-    //     answer: "http",
-    //     answers: [
-    //         "request",
-    //         "http",
-    //         "net",
-    //         "socket"
-    //     ]
-    // },
-    // {
-    //     question: "What does the 304 status code in HTTP signify?",
-    //     answer: "Not Modified",
-    //     answers: [
-    //         "Bad Request",
-    //         "Not Modified",
-    //         "Forbidden",
-    //         "Not Found"
-    //     ]
-    // },
-    // {
-    //     question: "In React, what is the method to change a component's state?",
-    //     answer: "setState()",
-    //     answers: [
-    //         "changeState()",
-    //         "modifyState()",
-    //         "updateState()",
-    //         "setState()"
-    //     ]
-    // },
-    // {
-    //     question: "What does SQL stand for?",
-    //     answer: "Structured Query Language",
-    //     answers: [
-    //         "Simple Query Language",
-    //         "Structured Query Language",
-    //         "Standard Query Language",
-    //         "Secure Query Language"
-    //     ]
-    // },
-    // {
-    //     question: "In MongoDB, what is a 'Document' equivalent to in SQL?",
-    //     answer: "A row in a table",
-    //     answers: [
-    //         "A table",
-    //         "A database",
-    //         "A row in a table",
-    //         "A column in a table"
-    //     ]
-    // },
-    // {
-    //     question: "Which CSS layout model introduces a two-dimensional grid system to the web?",
-    //     answer: "CSS Grid Layout",
-    //     answers: [
-    //         "Flexbox",
-    //         "CSS Grid Layout",
-    //         "Box Model",
-    //         "Positioning"
-    //     ]
-    // },
-    // {
-    //     question: "What is the purpose of Docker in development?",
-    //     answer: "Creating, deploying, and running applications in isolated environments (containers)",
-    //     answers: [
-    //         "Version control",
-    //         "Text editing",
-    //         "Creating, deploying, and running applications in isolated environments (containers)",
-    //         "Browser testing"
-    //     ]
-    // },
-    // {
-    //     question: "What does REST stand for in the context of API design?",
-    //     answer: "Representational State Transfer",
-    //     answers: [
-    //         "Real-time Simple Transfer",
-    //         "Representational State Transfer",
-    //         "Reliable Session Transfer",
-    //         "Remote Server Transfer"
-    //     ]
-    // },
-    // {
-    //     question: "In Angular, what is a Directive?",
-    //     answer: "A class with a @Directive decorator that extends HTML behavior",
-    //     answers: [
-    //         "A method to route paths",
-    //         "A server-side script",
-    //         "A CSS preprocessor",
-    //         "A class with a @Directive decorator that extends HTML behavior"
-    //     ]
-    // },
-    // {
-    //     question: "What is the main purpose of a JWT (JSON Web Token)?",
-    //     answer: "To securely transmit information between parties as a JSON object",
-    //     answers: [
-    //         "To encrypt passwords",
-    //         "To manage user sessions",
-    //         "To securely transmit information between parties as a JSON object",
-    //         "To store data in local storage"
-    //     ]
-    // },
-    // {
-    //     question: "In Redux, what is an 'Action'?",
-    //     answer: "An object that describes what happened",
-    //     answers: [
-    //         "A function that returns the new state",
-    //         "A method to dispatch state changes",
-    //         "An object that describes what happened",
-    //         "A component that handles state"
-    //     ]
-    // },
-    // {
-    //     question: "What is the main use of TypeScript in web development?",
-    //     answer: "To provide static typing for JavaScript",
-    //     answers: [
-    //         "To compile code to machine language",
-    //         "To provide static typing for JavaScript",
-    //         "To manage server-side scripting",
-    //         "To enhance CSS styling"
-    //     ]
-    // },
-    // {
-    //     question: "In Git, what is the command to create a new branch?",
-    //     answer: "git branch [branch-name]",
-    //     answers: [
-    //         "git make branch [branch-name]",
-    //         "git new branch [branch-name]",
-    //         "git branch [branch-name]",
-    //         "git create [branch-name]"
-    //     ]
-    // },
-    // {
-    //     question: "What does 'CI/CD' stand for in DevOps?",
-    //     answer: "Continuous Integration/Continuous Delivery",
-    //     answers: [
-    //         "Continuous Integration/Continuous Deployment",
-    //         "Continuous Implementation/Continuous Development",
-    //         "Continuous Integration/Continuous Delivery",
-    //         "Constant Integration/Constant Deployment"
-    //     ]
-    // },
-    // {
-    //     question: "In web development, what does 'SEO' stand for?",
-    //     answer: "Search Engine Optimization",
-    //     answers: [
-    //         "Search Engine Optimization",
-    //         "Server Endpoint Output",
-    //         "Simple Element Object",
-    //         "Secure Encryption Operation"
-    //     ]
-    // },
-    // {
-    //     question: "What is a 'Promise' in JavaScript?",
-    //     answer: "An object representing the eventual completion or failure of an asynchronous operation",
-    //     answers: [
-    //         "A data type similar to an array",
-    //         "A function that executes conditionally",
-    //         "A loop that runs until a condition is met",
-    //         "An object representing the eventual completion or failure of an asynchronous operation"
-    //     ]
-    // },
-    // {
-    //     question: "In web security, what is 'XSS'?",
-    //     answer: "Cross-Site Scripting",
-    //     answers: [
-    //         "Cross-Server Security",
-    //         "Cross-Site Scripting",
-    //         "Extended Style Sheets",
-    //         "Exclusive Server Setup"
-    //     ]
-    // }
-    
-]
+// function to hide all other cards except whichever one is being passed inside
+function hideOtherCards(displayCard){
+  let elements = document.querySelectorAll('.quiz-section');
+  elements.forEach(item => {
+    if (displayCard === item){
+      displayCard.classList.remove('hide');
+      displayCard.classList.add('unhide');
+    } else {
+      item.classList.remove('unhide')
+      item.classList.add('hide')
+    }
+  });
+}
+// function to log the highscore
+function logHighscore() {
+    const userInitials = document.querySelector("#initials").value;
+    // highscores.push({ score: score, initials: userInitials});
+    console.log(userInitials);
+    console.log(score);
+    // console.log(highscores);
+}
 
+// function to display Highscore
+function displayHighscore(scoreText) {
+    nameText = document.querySelector("#initials").value;
 
-let totalTime = 120;
+    if(scoreText != 0){
+    const highscoreList = document.querySelector('#highscores-list');
+    const li = document.createElement('li');
+    const nameSpan = document.createElement('span');
+    nameSpan.classList.add('nameText', 'highscoreText');
+    nameSpan.textContent = nameText;
+    li.appendChild(nameSpan);
+
+    const scoreSpan = document.createElement('span');
+    scoreSpan.classList.add('scoreText', 'highscoreText');
+    scoreSpan.textContent = scoreText;
+    li.appendChild(scoreSpan);
+
+   highscoreList.appendChild(li);}
+}
+// function to clear highscores
+function clearHighscores(){
+
+}
+
+let questionNum = 0;
+const text = document.querySelector(".answer-text");
+let score;
+
+// Timer Variables 
+let startingTime = 120;
 let timePenalty = 10;
-let timerDisplay = document.getElementById('time');
+let timer;
+let timerToggle = true;
+const timerDisplay = document.getElementById("time");
 
-document.addEventListener('DOMContentLoaded', function() {
+// HTML Section Elements
+const initialsCard = document.querySelector("#initials-container");
+const highscoreCard = document.querySelector("#highscores-container");
+const questionCard = document.querySelector("#quiz-question-container");
+const quizRulesCard = document.querySelector(".quiz-rules");
 
+// Buttons
+const startButton = document.getElementById("start-quiz");
+const submitButton = document.getElementById("submit-button");
+const goBackButton = document.querySelector("#go-back");
+const highScoreButton = document.querySelector('#high-scores');
+const clearHighscoresButton = document.querySelector('#clear-scores');
 
-    const initialsContainer = document.getElementById('initials-container');
-    const highscoreContainer = document.getElementById('highscores-container');
-    console.log(highscoreContainer);
+hideOtherCards(quizRulesCard);
+injectQuestion(quizQuestions[questionNum]);
 
-const startButton = document.getElementById('start-quiz');
-console.log(questionNum);
-const submitButton = document.getElementById('submit-button');
-
-
-
-submitButton.addEventListener("click", function(){
-    initialsContainer.style.display = "none";
-    highscoreContainer.style.display = "block";
-})
-
-let firstQuestion = quizQuestions[questionNum];
-startButton.addEventListener('click', function(){
-    const quizRules = document.querySelector('.quiz-rules');
-    const questionContainer = document.getElementById('quiz-question-container');
-    quizRules.style.display = 'none'; 
-    questionContainer.style.display = "block";
-    startTimer();
+highScoreButton.addEventListener("click", function(){
+  hideOtherCards(highscoreCard);
 });
-
-// for (let questionNum = 0; questionNum < 2; ){
-    console.log(quizQuestions[questionNum].answers);
-    injectQuestion(firstQuestion, quizQuestions[questionNum].answers);
-   
-// }
-
-
+goBackButton.addEventListener("click", function () {
+  hideOtherCards(quizRulesCard);
+});
+submitButton.addEventListener("click", function () {
+  logHighscore();
+  displayHighscore(totalTime);
+  hideOtherCards(highscoreCard);
+});
+clearHighscoresButton.addEventListener('click', function(){
+  clearHighscores();
+});
+startButton.addEventListener("click", function () {
+  score = 0;
+  restartQuiz();
 });
 
 /* 
+
+have timer toggle so initials doesnt show when time is at 0 and u r not answering questions 
 
 display quiz section
 we will have object with quiz questions and answers
