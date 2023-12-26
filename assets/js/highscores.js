697e9ed56e976fd6c35864d6f4aca3e24e5fecd8
cjswayne <134882908+cjswayne@users.noreply.github.com>
2023-12-26 10:16:49 -0800

    initial save

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,53 +1,50 @@
-const highscoreList = document.querySelector('#highscores-list');
-displayHighscore()
+const highscoreList = document.querySelector("#highscores-list");
+displayHighscore();
 function displayHighscore() {
-let highscoresList = localStorage.getItem('highscoresList');
-console.log(highscoresList);
-if (highscoresList) {
-let highscores = JSON.parse(highscoresList).map(score => JSON.parse(score));
-console.log(highscores);
-highscores.sort((a, b) => b.score - a.score);
+  let highscoresList = localStorage.getItem("highscoresList");
+  console.log(highscoresList);
+  if (highscoresList) {
+    let highscores = JSON.parse(highscoresList).map((score) =>
+      JSON.parse(score)
+    );
+    console.log(highscores);
+    highscores.sort((a, b) => b.score - a.score);
 
-highscoreList.classList.add('unhide')
-highscoreList.classList.remove('hide')
+    highscoreList.classList.add("unhide");
+    highscoreList.classList.remove("hide");
 
-// Clear existing list items
-highscoreList.innerHTML = '';
+    highscoreList.innerHTML = "";
 
-highscores.forEach((score) => {
-//   score = JSON.parse(score);
-//   console.log(score);
-  const li = document.createElement('li');
-  const nameSpan = document.createElement('span');
-  nameSpan.classList.add('nameText', 'highscoreText');
-  nameSpan.textContent = score.initials;
-  li.appendChild(nameSpan);
+    highscores.forEach((score) => {
+      const li = document.createElement("li");
+      const nameSpan = document.createElement("span");
+      nameSpan.classList.add("nameText", "highscoreText");
+      nameSpan.textContent = score.initials;
+      li.appendChild(nameSpan);
 
-  const scoreSpan = document.createElement('span');
-  scoreSpan.classList.add('scoreText', 'highscoreText');
-  scoreSpan.textContent = score.score;
-  li.appendChild(scoreSpan);
+      const scoreSpan = document.createElement("span");
+      scoreSpan.classList.add("scoreText", "highscoreText");
+      scoreSpan.textContent = score.score;
+      li.appendChild(scoreSpan);
 
-  highscoreList.appendChild(li);
-});
+      highscoreList.appendChild(li);
+    });
+  }
 }
-}
-const clearHighscoresButton = document.querySelector('#clear-scores');
+const clearHighscoresButton = document.querySelector("#clear-scores");
 const goBackButton = document.querySelector("#go-back");
 
-clearHighscoresButton.addEventListener('click', function(){
-    clearHighscores();
-  });
+clearHighscoresButton.addEventListener("click", function () {
+  clearHighscores();
+});
 
-  goBackButton.addEventListener("click", function () {
-    window.location = '../../index.html';
-  });
+goBackButton.addEventListener("click", function () {
+  window.location = "../../index.html";
+});
 
-  
-function clearHighscores(){
-    localStorage.clear()
-    
-    highscoreList.classList.remove('unhide')
-    highscoreList.classList.add('display-none')
-    }
-    
+function clearHighscores() {
+  localStorage.clear();
+
+  highscoreList.classList.remove("unhide");
+  highscoreList.classList.add("display-none");
+}
